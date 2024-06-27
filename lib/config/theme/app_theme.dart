e558d328df0e80cc4ebb1cd7efd3b5fab5d7fb54
Karla038 <karladk038@gmail.com>
2024-06-26 18:44:49 -0600

    Temas de la aplicacion

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.red,
+  Colors.orange,
+  Colors.yellow,
+  Colors.pinkAccent,
+  Colors.deepPurpleAccent
+];
+
+class AppTheme {
+
+  final int selectedColor;
+
+  AppTheme({
+    required this.selectedColor
+    }): assert (
+      selectedColor >= 0, 'Selecciona un color mayor a 0 '
+    ),
+    assert (
+      selectedColor < colorList.length,
+      'El color seleccionado debe ser menor ${ colorList.length -1}'
+    );
+
+    ThemeData getTheme() => ThemeData(
+      colorSchemeSeed: colorList[selectedColor]
+    );
+
+}
